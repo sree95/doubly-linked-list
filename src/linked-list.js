56b09f2ae0e14beee7acf576e651b24313f13400
PRINCETON\Sreenivas_Parimi <sreenivas_parimi@epam.com>
2017-03-15 18:08:53 +0530

    Added ctor,append,head and tail

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,13 +1,48 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
 
-    append(data) {}
+    constructor() {
+        this.length = 0;
+        this._head = null;
+        this._tail = null;   
+    }
 
-    head() {}
+    append(data) {
+        
+        var myNode = new Node(data,null,null); 
 
-    tail() {}
+        if(this.length === 0){
+            this._head = myNode;                        
+            this._tail = myNode;
+        }else{
+            // this._tail.next = myNode;
+            // this.prev = this._tail;
+            // this._tail = myNode;
+
+            this._tail = this._tail.next = {
+            value: data,
+            next: this,
+            prev: this.prev
+    }
+
+
+        this.length++;
+        return this;
+    }
+}
+
+    head() {
+        if (this.length > 0) {
+            return this._head;
+        }
+    }
+
+    tail() {
+        if (this.length > 0) {
+            return this._tail;
+        }
+    }
 
     at(index) {}
 
